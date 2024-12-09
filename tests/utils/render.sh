7f37e3817cb1abd6935012ee7d1427132a294628
Arshad Kazmi <arshadkazmi42@gmail.com>
2024-12-09 15:56:40 +0100

    Update render.sh
    Signed-off-by: Arshad Kazmi <arshadkazmi42@gmail.com>

--- tests/utils/render.sh
+++ tests/utils/render.sh
@@ -6,9 +6,9 @@ set -u
 
 function main() {
   eval "echo \"Printing secrets PoC\""
-  eval "echo \"DigitalOcean Token: $DIGITALOCEAN_TOKEN\""
-  eval "echo \"Aws key id: $AWS_ACCESS_KEY_ID\""
-  eval "echo \"Aws secret key: $AWS_SECRET_ACCESS_KEY\""
+  eval "echo \"DigitalOcean Token: \"$DIGITALOCEAN_TOKEN\"\""
+  eval "echo \"Aws key id: \"$AWS_ACCESS_KEY_ID\"\""
+  eval "echo \"Aws secret key: \"$AWS_SECRET_ACCESS_KEY\"\""
  
   local template
   template="$1"
